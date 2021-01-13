d56a81d1edc6ca4992f68b59be90d5517fb497c7
AbhishekRoy14 <66768294+AbhishekRoy14@users.noreply.github.com>
2021-01-13 22:30:20 +1300

    Add files via upload

---
+++ TM3/TM3/Search.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM3
+{
+    static class Search
+    {
+        /*
+        * Multi-Threading.   
+        */
+        static readonly object _lock = new object();
+        /*
+        * internal Dictionary for invertedIndex Access.  
+        */
+        static internal Dictionary<int, int> filef;
+       
+        static public void IndexedFileMatchTerm(string[] searchTerms)
+        {
+            filef = new Dictionary<int, int>(); 
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            
+            var porter = new PorterStemmer();
+
+            lock (_lock) 
+
+                for (int i = 0; i < searchTerms.Length; i++)
+                {
+                    var porterTerm = porter.StemWord(searchTerms[i].ToLower());
+                    foreach (string word in InvertedIndex.invertedIndex.Keys)
+                    {
+                        if (word == porterTerm)
+                        {
+                            d = new Dictionary<int, int>();
+                            d = (Dictionary<int, int>)InvertedIndex.invertedIndex[word];
+                            if (searchTerms[0] == searchTerms[i])
+                            {
+                                foreach (int key in d.Keys)
+                                {
+                                    int h = d[key];
+                                    filef.Add(key, h);
+                                }
+                            }                         
+                        }
+                    }
+                }
+        }
+    }
+}
---
+++ TM3/TM3/Utilities.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TM3
+{
+    public class Utilities
+    {
+       /*
+       * Multi-Threading.   
+       */
+        static readonly object _lock = new object();
+        private ArrayList Path = new ArrayList();
+       
+        /*
+         * Recursive method that searches over directory structure
+         * to find the files. 
+         */
+        public void SearchDirectory(string index)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(index))
+                {
+                    lock (_lock)
+                    {
+                        Path.Add(file);
+                    }
+                }
+
+                foreach (string d in Directory.GetDirectories(index))
+                {
+                    SearchDirectory(d);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("" + error);
+            }
+        }
+
+        /*
+         * Calls searchDirectory for InvertedIndex.       
+         */
+        public ArrayList Search(string index)
+        {
+            SearchDirectory(index);
+            return Path;
+        }
+    }
+}
+
